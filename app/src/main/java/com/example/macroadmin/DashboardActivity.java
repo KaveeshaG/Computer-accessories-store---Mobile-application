3f63d6480c06d42f99a0df84346a4515d379e97f
Kaveesha <uakaveeshagimhana@gmail.com>
2019-08-06 00:19:27 +0530

    with dashboard

---
+++ app/src/main/java/com/example/macroadmin/DashboardActivity.java
@@ -0,0 +1,5 @@
+package com.example.macroadmin;
+
+public class DashboardActivity {
+
+}
--- app/src/main/java/com/example/macroadmin/LoginActivity.java
+++ app/src/main/java/com/example/macroadmin/LoginActivity.java
@@ -2,13 +2,16 @@ package com.example.macroadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
     ImageView backBtn;
+    Button loginSBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,16 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        loginSBtn = findViewById(R.id.loginSBtn);
+
+        loginSBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
             }
         });
     }

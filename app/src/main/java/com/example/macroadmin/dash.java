55dc75301f266c24a1b5aa67d4c5ca43b1647696
Kaveesha <uakaveeshagimhana@gmail.com>
2019-08-06 03:35:27 +0530

    fine tunes with employee

--- app/src/main/java/com/example/macroadmin/dash.java
+++ app/src/main/java/com/example/macroadmin/dash.java
@@ -1,6 +1,7 @@
 package com.example.macroadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,13 +12,27 @@ import android.widget.ImageView;
 public class dash extends AppCompatActivity {
 
     ImageView propic;
+    CardView employee;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
 
+
+
         propic = findViewById(R.id.propicd);
+
+        propic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(dash.this, profile.class);
+                startActivity(intent);
+            }
+        });
+
+        employee = findViewById(R.id.employee);
 
         propic.setOnClickListener(new View.OnClickListener() {
             @Override
